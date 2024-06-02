e8a8c5e65d732971a11262723cebb330739fcbf4
aungbhonekhant <aungbhonekhant.dev@gmail.com>
2024-06-02 16:42:16 +0700

    feat: crate Footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,8 @@
+"use client";
+export const Footer = () => {
+    return (
+        <footer className="pt-10 bg-slate-800 text-white">
+            <p className="text-center pb-4 text-slate-400">Copyrights © 2024, FazWaz Group </p>
+        </footer>
+    )
+}
