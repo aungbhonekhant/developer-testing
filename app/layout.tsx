ff423f0d297feb22c2c30a6bed892e1f07f58df3
aungbhonekhant <aungbhonekhant.dev@gmail.com>
2024-06-02 16:43:14 +0700

    feat: implement components in layout and page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Navigation } from "@/components/Navigation";
+import { Footer } from "@/components/Footer";
+import { Provider } from "@/components/Provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +19,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Provider>
+          <Navigation />
+            {children}
+          <Footer />
+        </Provider>
+        </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
-import Image from "next/image";
+"use client"
+import { Hero } from "@/components/Hero";
+import { PropertyList } from "@/components/PropertyList";
+import { useState } from "react";
 
 export default function Home() {
+  const [filter, setFilter] = useState({})
   return (
-    <div></div>>
+    <>
+      <Hero setFilter={setFilter}/>
+      <PropertyList filter={filter}/>
+    </>
   );
 }
