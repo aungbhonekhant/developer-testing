db80d5fe98990acb9844a90ac225dcf47244f6b8
aungbhonekhant <aungbhonekhant.dev@gmail.com>
2024-06-02 16:41:29 +0700

    feat: crate pagination component for properties list display

---
+++ components/Pagination.tsx
@@ -0,0 +1,86 @@
+"use client";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationLink,
+} from "./ui/pagination";
+import { Button } from "./ui/button";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+
+type Props = {
+    pageCount: number;
+    currentPage: number;
+    setPage: Function
+};
+
+const PaginationComponent = ({ pageCount, currentPage, setPage }: Props) => {
+ 
+  const generatePaginationLinks = () => {
+    const paginationLinks = [];
+    const leftEllipsis = currentPage > 2;
+    const rightEllipsis = currentPage < pageCount - 1;
+
+    for (let i = 1; i <= pageCount; i++) {
+      if (
+        i === 1 ||
+        i === pageCount ||
+        (i >= currentPage - 1 && i <= currentPage + 1)
+      ) {
+        paginationLinks.push(
+          <PaginationLink
+            key={i}
+            onClick={() => changePage(i)}
+            isActive={currentPage === i}
+          >
+            {i}
+          </PaginationLink>
+        );
+      }
+    }
+
+    if (leftEllipsis) {
+      paginationLinks.splice(1, 0, <PaginationEllipsis key="left" />);
+    }
+    if (rightEllipsis) {
+      paginationLinks.splice(
+        paginationLinks.length - 1,
+        0,
+        <PaginationEllipsis key="right" />
+      );
+    }
+
+    return paginationLinks;
+  };
+
+  const changePage = (page: number) => {
+    setPage(page)
+  };
+  return (
+    <Pagination>
+      <PaginationContent className=" *:cursor-pointer">
+        <Button
+          variant="ghost"
+          disabled={currentPage <= 1}
+          onClick={() => changePage(currentPage - 1)}
+          className="group"
+        >
+          <ChevronLeft className="group-hover:-translate-x-1 transition-all duration-300 delay-150" />{" "}
+          Previous
+        </Button>
+        {generatePaginationLinks()}
+        <Button
+          variant="ghost"
+          disabled={currentPage === pageCount}
+          onClick={() => changePage(currentPage + 1)}
+          className="group"
+        >
+          Next{" "}
+          <ChevronRight className="group-hover:translate-x-1 transition-all duration-300 delay-150" />
+        </Button>
+      </PaginationContent>
+    </Pagination>
+  );
+};
+
+export default PaginationComponent;
