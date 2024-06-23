b2e6216f250c28dd23466d71848ced20e9b030bd
aungbhonekhant <aungbhonekhant.dev@gmail.com>
2024-06-23 10:49:34 +0700

    fix: use next Image for hero background image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,13 +1,22 @@
 "use client";
 import { SearchForm } from "./SearchForm"
+import Image from "next/image";
 
 type Props = {
     setFilter: Function
 }
 export const Hero = ({setFilter}: Props) => {
     return (
-        <div className="min-h-screen hero flex justify-center items-center">
-            <div className="max-w-6xl flex flex-col gap-6 items-center pb-10">
+        <div className="min-h-screen flex justify-center items-center">
+                <Image 
+                    src="/home.jpeg"
+                    fill
+                    alt="Hero section background image"
+                    objectFit="cover"
+                    loading="lazy"
+                    className="-z-10"
+                />
+            <div className="max-w-6xl flex flex-col gap-6 items-center pb-10 p-2">
                 <div className="max-w-4xl space-y-5">
                     <h1 className="text-3xl sm:text-4xl lg:text-6xl font-bold text-center text-white">Thailand's Home for Real Estate</h1>
                     <p className="text-slate-300 text-center">The leading real estate marketplace to buy, rent and sell property in Thailand</p>
