7abf61f0789a76755aaafdaf7095b9e35cb7fe87
aungbhonekhant <aungbhonekhant.dev@gmail.com>
2024-06-23 10:48:16 +0700

    fix: import property list with dynamic import

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 "use client"
 import { Hero } from "@/components/Hero";
-import { PropertyList } from "@/components/PropertyList";
 import { useState } from "react";
-
+import dynamic from 'next/dynamic';
+const PropertyList = dynamic(()=>import("@/components/PropertyList").then(mod=>mod.PropertyList))
 export default function Home() {
   const [filter, setFilter] = useState({})
   return (
